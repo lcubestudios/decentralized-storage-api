45017e7dbe8d1aa8f3c88449d36e8ed565fb7a0b
Luis Munoz <lmunoz0806@gmail.com>
2022-04-10 17:11:45 -0400

    returning Json error message

--- trigger.php
+++ trigger.php
@@ -4,11 +4,11 @@ $method = $_SERVER['REQUEST_METHOD'];
 switch ($method) {
     case 'GET':
         $sh = shell_exec("python3 ListFiles.py");
-        echo(json_encode($sh));
+        echo json_encode($sh);
         break;
     default:
-        $message = "Error send a server method";
-        echo($message);
+        $message = {'message': "Error send a server method"};
+        echo json_encode($message);
         break;
 }
 
