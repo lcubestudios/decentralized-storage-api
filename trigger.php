16a5bf2683a0b16c186612d0630465aa057a3584
Luis Munoz <lmunoz0806@gmail.com>
2022-04-10 17:13:42 -0400

    debug: testing with bracket

--- trigger.php
+++ trigger.php
@@ -4,11 +4,7 @@ $method = $_SERVER['REQUEST_METHOD'];
 switch ($method) {
     case 'GET':
         $sh = shell_exec("python3 ListFiles.py");
-        echo json_encode($sh);
-        break;
-    default:
-        $message = {'message': "Error send a server method"};
-        echo json_encode($message);
+        echo(json_encode($sh));
         break;
 }
 
