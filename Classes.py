79c0cea521694e5651d7a06398be100dc7a6a119
Luis Munoz <lmunoz0806@gmail.com>
2022-04-10 16:09:31 -0400

    chore(upload): json was added and debuging upload

--- Classes.py
+++ Classes.py
@@ -4,6 +4,9 @@ from uplink_python.module_classes import ListObjectsOptions, Permission, SharePr
 from uplink_python.uplink import Uplink
 from dotenv import load_dotenv
 import os
+import sys
+import requests
+import json
 
 # LOAD CONTAINER VARIABLES
 load_dotenv()
@@ -11,12 +14,11 @@ load_dotenv()
 class Credentials():
     def __init__(self):
         # Storj configuration information
-        global api_key, satellite, encryption_passphrase, bucket, storj_file_name, src_full_name, destination_full_filename
+        global api_key, satellite, encryption_passphrase, bucket, src_full_name, destination_full_filename
         api_key =  os.getenv('API_KEY')
         satellite = os.getenv('SATELLITE')
         encryption_passphrase = os.getenv('ENCRYPTION_PASSPHRASE')
         bucket = "uplink"
-        storj_file_name = "luis/ipfs.png" # (path + filename) OR filename
         src_full_name = "/Users/cloudninja/Desktop/ipfs.png" # Source and destination path and file name for testing
         destination_full_filename ="/Users/cloudninja/Desktop/ipfs3.png "
 
@@ -48,9 +50,12 @@ class Methods():
             bucket_list = project.list_buckets()
             for bucket in bucket_list:
                 # as python class object
-                print(bucket.name, " | ", datetime.fromtimestamp(bucket.created))
-                # as python dictionary
-                print(bucket.get_dict())
+                # print(bucket.name, " | ", datetime.fromtimestamp(bucket.created))
+                # # as python dictionary
+                # print(bucket.get_dict())
+                data = bucket.get_dict()
+                json_formatted_str = json.dumps(data, indent=4, sort_keys=True)
+                print(json_formatted_str)
             print("Buckets listing: COMPLETE!")
             #
         except StorjException as exception:
@@ -64,31 +69,44 @@ class Methods():
                                                                             system=True))
             # print all objects path
             for obj in objects_list:
-                print(obj.key, " | ", obj.is_prefix)  # as python class object
-                print(obj.get_dict())  # as python dictionary
+                # print(obj.key, " | ", obj.is_prefix)  # as python class object
+                # print(obj.get_dict())  # as python dictionary
+                data = obj.get_dict()
+                json_formatted_str = json.dumps(data, indent=4, sort_keys=True)
+                print(json_formatted_str)
             print("Objects listing: COMPLETE!")
             #
         except StorjException as exception:
             print("Exception Caught: ", exception.details)
 
-    def UploadObject(self,project):
+    def UploadObject(self,project,file_item):
         try:
             # upload file/object
             print("\nUploading data...")
             # get handle of file to be uploaded
-            file_handle = open(src_full_name, 'r+b')
-            # get upload handle to specified bucket and upload file path
-            upload = project.upload_object(bucket, storj_file_name)
-            #
-            # upload file on storj
-            upload.write_file(file_handle)
-            #
-            # commit the upload
-            upload.commit()
-            # close file handle
-            file_handle.close()
-            print("Upload: COMPLETE!")
-            #
+            # file_handle = open(src_full_name, 'r+b')
+            # check if the file has been uploaded
+            if file_item.filename:
+                # strip the leading path from the file name
+                fn = os.path.basename(file_item.filename)
+                
+                # open read and write the file into the server
+                file_handle = open(fn, 'r+b')#.write(file_item.file.read())
+
+                # get upload handle to specified bucket and upload file path
+                upload = project.upload_object(bucket, file_item.filename)
+                #
+                # upload file on storj
+                upload.write_file(file_handle)
+                #
+                # commit the upload
+                upload.commit()
+                # close file handle
+                file_handle.close()
+                print("Upload: Complete!")
+                #
+            else:
+                print("Upload Failed")
         except StorjException as exception:
             print("Exception Caught: ", exception.details)
     
@@ -99,7 +117,7 @@ class Methods():
             # get handle of file which data has to be downloaded
             file_handle = open(destination_full_filename, 'w+b')
             # get download handle to specified bucket and object path to be downloaded
-            download = project.download_object(bucket, storj_file_name)
+            download = project.download_object(bucket, file_name)
             #
             # download data from storj to file
             download.read_file(file_handle)
--- Upload.py
+++ Upload.py
@@ -1,8 +1,19 @@
 from distutils.command.upload import upload
 from Classes import *
 
+
 creds = Credentials()
 method = Methods()
 
+url = 'http://127.0.0.1:5500/'
+headers = {
+    "Content-Type":"text",
+}
+r = requests.post(url, headers=headers,allow_redirects=True)
+
 project = method.StablishConnection()
-upload_object = method.UploadObject(project)
+file_item = form['filename']
+upload_object = method.UploadObject(project,file_item)
+
+
+
