75989e95f72523f17459b787121df1b02b70d9f1
Luis Munoz <lmunoz0806@gmail.com>
2022-04-11 00:06:28 -0400

    test(upload): testing upload

--- Upload.py
+++ Upload.py
@@ -1,22 +1,13 @@
 #!/usr/bin/python3
 from Classes import *
-
 creds = Credentials()
 method = Methods()
-
-headers = {
-    'Content-Type': 'application/json', 
-    'Access-Control-Allow-Origin': 'http://localhost:5500', 
-    'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
-    'Access-Control-Allow-Headers':'Content-Type, Authorization, Accept, Accept-Language, X-Authorization'
-}
 # Create instance of FieldStorage 
 form = cgi.FieldStorage() 
 # Get data from fields
 file_item = form.getvalue['filename']
-print(file_item)
-# project = method.StablishConnection()
-# upload_object = method.UploadObject(project,file_item)
+project = method.StablishConnection()
+upload_object = method.UploadObject(project,file_item)
 
 
 
