78b1f91884fda422284c530589c65df7ce7e9e54
Luis Munoz <lmunoz0806@gmail.com>
2022-04-11 22:45:09 -0400

    chore(upload): changing read binary to write binary on upload

--- Classes.py
+++ Classes.py
@@ -97,7 +97,7 @@ class Methods():
                 fn = os.path.basename(file_item.filename)
                 
                 # open read and write the file into the server
-                file_handle = open(fn, 'r+b').write(file_item.file.read())
+                file_handle = open(fn, 'wb').write(file_item.file.read())
 
                 # get upload handle to specified bucket and upload file path
                 upload = project.upload_object(bucket, file_item.filename)
